6921bc617ffcf448050f0b8916d87e0cc89e2c0f
Abraham Vallejos <aronpc1997@gmail.com>
2023-08-08 12:42:49 -0500

    [Abraham]:Fixed assigned new "apiUrl" in environments.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   hmr: false,
-  apiUrl: 'http://209.145.61.41:8002',
+  apiUrl: 'https://api-coop-test.bigpuntos.com',
   setKey: '6LdGH6glAAAAALbaMNNz7mIzmWQH3sdfiUGHvyal'
 };
 
